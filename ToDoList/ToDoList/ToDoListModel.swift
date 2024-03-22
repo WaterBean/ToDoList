48583762625ae62565b7925d1bafca01b08d5afa
WaterBean <gkstnqls9@gmail.com>
2024-03-22 18:06:54 +0900

    Create Frame

---
+++ ToDoList/ToDoList/ToDoListModel.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoListModel.swift
+//  ToDoList
+//
+//  Created by 한수빈 on 3/22/24.
+//
+
+import Foundation
+
+struct ToDoListModel {
+    var name: [String]
+    var detail: [String]
+    //var image: []
+    
+    
+}
---
+++ ToDoList/ToDoList/ToDoListViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ToDoListViewController.swift
+//  ToDoList
+//
+//  Created by 한수빈 on 3/22/24.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+
+    
+    var sectionList : [String] = ["할 일"]
+    var reuseIdentifier = "todolist"
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return sectionList.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        var config = cell.defaultContentConfiguration()
+        
+        config.text = "\(indexPath.row)"
+        config.secondaryText = "힘든데\(indexPath.row)"
+        config.image = .strokedCheckmark.withTintColor(.brown)
+        //config.image = .animatedImage(with: [.strokedCheckmark, .remove], duration: 1000)
+        
+        cell.contentConfiguration = config
+        return cell
+    }
+    
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionList[section]
+    }
+    
+    
+    
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        
+        return true
+    }
+    
+    
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            items.remove(at: (indexPath as NSIndexPath).row)
+        
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.backgroundColor = .blue
+    }
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        cell?.backgroundColor = .white
+    }
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
